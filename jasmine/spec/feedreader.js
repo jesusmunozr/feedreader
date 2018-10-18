f31b0ca8636df78b6450ab3b44dd60269bd1beb3
Jesus Munoz Ramirez <jmunoz@rosen-group.com>
2018-10-18 18:05:23 -0500

    Fees defined and url is not empty test ready

--- jasmine/spec/feedreader.js
+++ jasmine/spec/feedreader.js
@@ -31,6 +31,12 @@ $(function() {
          * in the allFeeds object and ensures it has a URL defined
          * and that the URL is not empty.
          */
+        it('should be defined and not empty', function() {
+            allFeeds.forEach(function(fee) {
+                expect(fee.url).toBeDefined();
+                expect(fee.url.trim().length).toBeGreaterThan(0);
+            });
+        });
 
 
         /* TODO: Write a test that loops through each feed
